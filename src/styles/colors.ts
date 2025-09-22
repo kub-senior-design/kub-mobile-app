805c0f1d73785a59e344199226f16361ae675c0b
Rudra Patel <patelrudra2003@gmail.com>
2025-09-21 20:54:22 -0400

    feat: Add icon and begin working on auth

---
+++ src/styles/colors.ts
@@ -0,0 +1,30 @@
+const colors = {
+  primary: "#00447c",
+  secondary: "#426f9a",
+
+  white: "#ffffff",
+  black: "#000000",
+
+  success: "#4b8847",
+  info: "#057cff",
+  warning: "#ffc107",
+  danger: "#dc3545",
+
+  green: "#4b8847",
+  orange: "#f70",
+  cyan: "#057cff",
+
+  gray: {
+    100: "#f8f9fa",
+    200: "#e9ecef",
+    300: "#dee2e6",
+    400: "#ced4da",
+    500: "#adb5bd",
+    600: "#6c757d",
+    700: "#495057",
+    800: "#343a40",
+    900: "#212529",
+  },
+} as const;
+
+export default colors;
